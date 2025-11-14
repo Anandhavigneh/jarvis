124bfc772ad453f0ad90ca019baf74e57c9b493a
Anandhavignesh <141007772+Anandhavigneh@users.noreply.github.com>
2025-11-14 11:35:35 +0530

    Make greetings generic in wishMe function
    Removed user-specific greetings and made them generic.

--- jarvis.py
+++ jarvis.py
@@ -19,12 +19,12 @@ def speak(audio):
 def wishMe():
     hour=int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
-        speak("Good Morning priyanga!")
+        speak("Good Morning !")
     elif hour>=12 and hour<18:
-        speak("Good Afternoon priyanga!")
+        speak("Good Afternoon !")
     else:
-        speak("Good Evening priyanga!")
-    speak("I am Jarvis. Please tell me how may I help you priyanga")
+        speak("Good Evening !")
+    speak("I am Jarvis. Please tell me how may I help you ")
 
 def takeCommand():
     r=sr.Recognizer()
